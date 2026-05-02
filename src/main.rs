652c8d8f18ac0cb7b09c92eefc6bfab4d32af3a3
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2026-05-02 16:27:26 +0900

    gpuiでcargo-packagerできるようにした。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use gpui::{
+    App, AppContext, Application, Bounds, Context, IntoElement, ParentElement, Render, Styled,
+    Window, WindowBounds, WindowOptions, div, px, rgb, size,
+};
+
+struct Main;
+
+impl Main {
+    fn new() -> Self {
+        Self
+    }
+}
+
+impl Render for Main {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .flex_col()
+            .items_center()
+            .justify_center()
+            .bg(rgb(0xffffff))
+            .text_2xl()
+            .w_full()
+            .h_full()
+            .child("Hello, World!")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Application::new().run(|cx: &mut App| {
+        let bounds = Bounds::centered(None, size(px(250.), px(700.)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |_, cx| cx.new(|_| Main::new()),
+        )
+        .unwrap();
+    })
 }
