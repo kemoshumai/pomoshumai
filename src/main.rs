0d5431311a5f67659633472c131f117a303c124e
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2026-06-18 15:37:51 +0900

    サイズ変更

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     Application::new().run(|cx: &mut App| {
         gpui_component::init(cx);
 
-        let bounds = Bounds::centered(None, size(px(520.), px(720.)), cx);
+        let bounds = Bounds::centered(None, size(px(480.), px(640.)), cx);
         cx.open_window(
             WindowOptions {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
